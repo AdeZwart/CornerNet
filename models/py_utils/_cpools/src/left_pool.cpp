218f3bc55f313b92026a52cc718b93d6f7db15e7
root <root@NL-EVI-1R8LZD3.valtech.com>
2023-04-26 15:41:29 +0200

    Update depencendies and fix cpools for updated api

--- models/py_utils/_cpools/src/left_pool.cpp
+++ models/py_utils/_cpools/src/left_pool.cpp
@@ -25,7 +25,7 @@ std::vector<at::Tensor> pool_forward(
         at::max_out(max_temp, input_temp, output_temp);
     }
 
-    return { 
+    return {
         output
     };
 }
@@ -41,8 +41,8 @@ std::vector<at::Tensor> pool_backward(
     int32_t height  = input.size(2);
     int32_t width   = input.size(3);
 
-    auto max_val = at::zeros(torch::CUDA(at::kFloat), {batch, channel, height});
-    auto max_ind = at::zeros(torch::CUDA(at::kLong),  {batch, channel, height});
+    auto max_val = at::zeros({batch, channel, width}, torch::CUDA(at::kFloat));
+    auto max_ind = at::zeros({batch, channel, width}, torch::CUDA(at::kLong));
 
     auto input_temp = input.select(3, width - 1);
     max_val.copy_(input_temp);
@@ -54,8 +54,8 @@ std::vector<at::Tensor> pool_backward(
     output_temp.copy_(grad_output_temp);
 
     auto un_max_ind = max_ind.unsqueeze(3);
-    auto gt_mask    = at::zeros(torch::CUDA(at::kByte),  {batch, channel, height});
-    auto max_temp   = at::zeros(torch::CUDA(at::kFloat), {batch, channel, height});
+    auto gt_mask    = at::zeros({batch, channel, width}, torch::CUDA(at::kByte));
+    auto max_temp   = at::zeros({batch, channel, width}, torch::CUDA(at::kFloat));
     for (int32_t ind = 1; ind < width; ++ind) {
         input_temp = input.select(3, width - ind - 1);
         at::gt_out(gt_mask, input_temp, max_val);
@@ -75,11 +75,11 @@ std::vector<at::Tensor> pool_backward(
 
 PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
     m.def(
-        "forward", &pool_forward, "Left Pool Forward", 
+        "forward", &pool_forward, "Left Pool Forward",
         py::call_guard<py::gil_scoped_release>()
     );
     m.def(
-        "backward", &pool_backward, "Left Pool Backward", 
+        "backward", &pool_backward, "Left Pool Backward",
         py::call_guard<py::gil_scoped_release>()
     );
 }
